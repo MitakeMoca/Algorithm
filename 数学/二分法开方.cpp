47ff5098e4a060a75ae2010cd55116d7ed6e8d56
MitakeMoca <1043530132@qq.com>
2025-05-19 20:26:30 +0800

    feat: binary sqrt

---
+++ 数学/二分法开方.cpp
@@ -0,0 +1,19 @@
+/*
+    使用二分法求出 sqrt(n) 的整数部分
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+ll my_sqrt(ll n) {
+    ll l = 0, r = n;
+    while (l < r) {
+        ll mid = (l + r) / 2;
+        if (mid * mid <= n) 
+            l = mid;
+        else 
+            r = mid;
+    }
+    return l;
+}
