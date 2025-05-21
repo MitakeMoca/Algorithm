fbd79f83c77969ecca864ed781c6a3d2b3e1d078
MitakeMoca <1043530132@qq.com>
2025-05-21 18:14:35 +0800

    quick pow

---
+++ 数学/快速幂.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+ll quickPow(ll a, ll b, ll c) {
+    ll ret = 1;
+    while(b) {
+        if(b & 1)
+            ret = ret * a % c;
+        a = a * a % c;
+        b >>= 1;
+    }
+    return ret;
+}
